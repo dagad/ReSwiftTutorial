8d9d91c6aa84145806c9eb89f9ff9a3a3116c219
dagad <dagad@estsoft.com>
2019-03-18 16:23:39 +0900

    create app store

--- MemoryTunes/AppDelegate.swift
+++ MemoryTunes/AppDelegate.swift
@@ -28,13 +28,15 @@
  * THE SOFTWARE.
  */
 
-import UIKit
+import ReSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
   
   var window: UIWindow?
   var appRouter: AppRouter?
+
+  var store = Store<AppState>(reducer: appReducer, state: nil)
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     
