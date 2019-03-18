33ab306f7c303d6a7bf8fd30484864fb8cf33ddb
dagad <dagad@estsoft.com>
2019-03-18 16:51:46 +0900

    edit AppRouter

--- MemoryTunes/AppDelegate.swift
+++ MemoryTunes/AppDelegate.swift
@@ -30,13 +30,13 @@
 
 import ReSwift
 
+var store = Store<AppState>(reducer: appReducer, state: nil)
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
   
   var window: UIWindow?
   var appRouter: AppRouter?
-
-  var store = Store<AppState>(reducer: appReducer, state: nil)
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     
--- MemoryTunes/AppRouter.swift
+++ MemoryTunes/AppRouter.swift
@@ -37,11 +37,39 @@ enum RoutingDestination: String {
 }
 
 final class AppRouter {
-  
+
   let navigationController: UINavigationController
-  
+
   init(window: UIWindow) {
     navigationController = UINavigationController()
     window.rootViewController = navigationController
+    // 1
+    store.subscribe(self) {
+      $0.select {
+        $0.routingState
+      }
+    }
+  }
+
+  // 2
+  fileprivate func pushViewController(identifier: String, animated: Bool) {
+    let viewController = instantiateViewController(identifier: identifier)
+    navigationController.pushViewController(viewController, animated: animated)
+  }
+
+  private func instantiateViewController(identifier: String) -> UIViewController {
+    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+    return storyboard.instantiateViewController(withIdentifier: identifier)
+  }
+}
+
+// MARK: - StoreSubscriber
+// 3
+extension AppRouter: StoreSubscriber {
+  func newState(state: RoutingState) {
+    // 4
+    let shouldAnimate = navigationController.topViewController != nil
+    // 5
+    pushViewController(identifier: state.navigationState.rawValue, animated: shouldAnimate)
   }
 }
